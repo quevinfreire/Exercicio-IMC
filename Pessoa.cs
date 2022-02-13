843ded005b5b7dc5404e3df2759f3250210bd060
Quevin Freire <quevinfreire93@gmail.com>
2022-02-12 20:50:07 -0400

    "Adicionando cadastro em arquivo txt"

--- Pessoa.cs
+++ Pessoa.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Text;
+using System.IO;
 namespace _04ExercicioIMC
 {
     public class Pessoa
     {   
         public string nome;
         public double peso, altura;
+        string caminho = "C:\\Codigos\\DadosUsuarioIMC.txt";
 
         public double imc()
         {
@@ -12,42 +15,70 @@ namespace _04ExercicioIMC
         }
         public string situacao(double imc)
         {
-            string retorno;
+            string retorno_S;
 
             if (imc < 18.5)
             {
-                retorno = "Abaixo do peso.";
+                retorno_S = "Abaixo do peso.";
             }
             else if (imc < 25)
             {
-                retorno = "Peso Normal.";
+                retorno_S = "Peso Normal.";
             }
             else if (imc < 30)
             {
-                retorno = "Acima do Peso.";
+                retorno_S = "Acima do Peso.";
             }
             else if (imc < 35)
             {
-                retorno = "Obesidade I";
+                retorno_S = "Obesidade I";
             }
             else if (imc < 40)
             {
-                retorno = "Obesidade II";
+                retorno_S = "Obesidade II";
             }
             else
             {
-                retorno = "Obesidade III";
+                retorno_S = "Obesidade III";
             }
-            return retorno;
+            return retorno_S;
         }   
         public void mensagem()
         {   
             double obterimc = imc();
             string obterSituacao = situacao(obterimc);
 
-            Console.WriteLine(nome+" você tem "+obterimc.ToString("F")+" de IMC. Sua situação é: "+obterSituacao);
+            Console.WriteLine(nome+", você tem "+obterimc.ToString("F")+" de IMC.");
+            Console.WriteLine();
+            Console.WriteLine("Sua situação é: "+obterSituacao);
+            Console.WriteLine();
             Console.WriteLine("Cálculo realizado: "+peso+" / ("+altura+" * "+altura+")");
         }
+
+        public void gravaDados()
+        {   
+            double obterimc = imc();
+            string obterSituacao = situacao(obterimc);
+            StreamWriter dados = new StreamWriter(caminho, true, Encoding.ASCII);
+
+            dados.WriteLine("Nome: "+nome);
+            dados.WriteLine("IMC: "+imc());
+            dados.WriteLine("Situacao: "+obterSituacao);
+
+            Console.WriteLine("Dados gravados com sucesso!");
+            dados.Close();
+        }
+        public void lerDados()
+        {
+            StreamReader dadosL = new StreamReader(caminho);
+
+            while(dadosL.EndOfStream!=true)
+            {
+                Console.WriteLine(dadosL.ReadLine());
+            }
+            dadosL.Close();
+        }
+
 
     }
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.IO;
 
 namespace _04ExercicioIMC
 {
@@ -6,16 +8,69 @@ namespace _04ExercicioIMC
     {
         static void Main(string[] args)
         {
+            string escolhaUsuario = "";
             Pessoa a = new Pessoa();
-            
-            Console.WriteLine("Qual é o seu nome?");
-            a.nome = Console.ReadLine();
-            Console.WriteLine("Qual é o seu peso?");
-            a.peso = double.Parse(Console.ReadLine());
-            Console.WriteLine("Qual é a sua altura?");
-            a.altura = double.Parse(Console.ReadLine());
 
-            a.mensagem();
+            Console.WriteLine();
+            Console.WriteLine("I- Verificar IMC");
+            Console.WriteLine("L- Ler dados cadastrados");
+            Console.WriteLine("S- Sair");
+            Console.WriteLine();
+            Console.WriteLine("Digite uma operação.");
+
+            escolhaUsuario = Console.ReadLine().ToUpper();
+
+            while (escolhaUsuario != "S")
+            {
+                if(escolhaUsuario == "I")
+                {   
+                    string escolhaUsuarioG = "";
+
+                    Console.WriteLine("Informe o seu nome: ");
+                    a.nome = Console.ReadLine();
+                    Console.WriteLine("Informe o seu peso: ");
+                    a.peso = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Informe a sua altura? ");
+                    a.altura = double.Parse(Console.ReadLine());
+                    Console.Clear();
+                    a.mensagem();
+                    Console.WriteLine();
+                    Console.WriteLine("Deseja gravar os dados? ");
+                    Console.WriteLine("Y- Para sim ");
+                    Console.WriteLine("N- Para não ");
+
+                    escolhaUsuarioG = Console.ReadLine().ToUpper();
+
+                    if (escolhaUsuarioG == "Y")
+                    {
+                        a.gravaDados();
+                    }
+                    else if (escolhaUsuarioG == "N")
+                    {
+                        Console.WriteLine("Dados não salvos!");
+                    }
+                }
+                else if(escolhaUsuario == "L")
+                {
+                    a.lerDados();
+                }
+                Console.WriteLine("Pressione uma tecla para continuar...");
+                Console.ReadKey();
+                Console.Clear();
+
+                Console.WriteLine();
+                Console.WriteLine("I- Verificar IMC");
+                Console.WriteLine("L- Ler dados cadastrados");
+                Console.WriteLine("S- Sair");
+                Console.WriteLine();
+                Console.WriteLine("Digite uma operação.");
+
+                escolhaUsuario = Console.ReadLine().ToUpper();
+
+                
+
+
+            }
         }
     }
 }
